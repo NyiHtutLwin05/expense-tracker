9255c2eaca4f76ca626a0f004d950c5af340a5a1
James <winleiei@Wins-MacBook-Air.local>
2024-08-11 15:17:38 +0630

    Finalizing the app

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,84 @@
 <template>
-  <Header/>
-   <div class="container">
-     <Balance :total ='total' />
-     <IncomeExpenses/>
-     <TransactionList :transactions="transactions"/>
-     <AddTransaction/>
-   </div>
+  <Header />
+  <div class="container">
+    <Balance :total="total" />
+    <IncomeExpenses :income="+income" :expenses="+expenses" />
+    <TransactionList
+      :transactions="transactions"
+      @transactionDeleted="handleTransactionDeleted"
+    />
+    <AddTransaction @transaction-submitted="handleTransactionSubmitted" />
+  </div>
 </template>
 <script setup>
-import Header from './components/Header.vue';
-import Balance from './components/Balance.vue';
-import IncomeExpenses from './components/IncomeExpenses.vue';
-import TransactionList from './components/TransactionList.vue';
-import AddTransaction from './components/AddTransaction.vue';
+import Header from "./components/Header.vue";
+import Balance from "./components/Balance.vue";
+import IncomeExpenses from "./components/IncomeExpenses.vue";
+import TransactionList from "./components/TransactionList.vue";
+import AddTransaction from "./components/AddTransaction.vue";
+import { useToast } from "vue-toastification";
+import { ref, computed, onMounted } from "vue";
 
-import {ref,computed} from 'vue';
+const toast = useToast();
 
-const transactions = ref([
-  {id: 1, text: 'Flower', amount: -19.99},
-  {id: 2, text: 'Salary', amount: 299.97},
-  {id: 3, text: 'Book', amount: -10},
-  {id: 4, text: 'Camera', amount: 250},
-]);
+const transactions = ref([]);
 
-const total = computed(()=>{
-  return transactions.value.reduce((acc,transaction)=>{
-    return acc + transaction.amount;
-  },0)
+onMounted(() => {
+  const savedTransactions = JSON.parse(localStorage.getItem("transactions"));
+  if (savedTransactions) {
+    transactions.value = savedTransactions;
+  }
 });
+
+// get total
+const total = computed(() => {
+  return transactions.value.reduce((acc, transaction) => {
+    return acc + transaction.amount;
+  }, 0);
+});
+// get income
+const income = computed(() => {
+  return transactions.value
+    .filter((transaction) => transaction.amount > 0)
+    .reduce((acc, transaction) => {
+      return acc + transaction.amount;
+    }, 0)
+    .toFixed(2);
+});
+// get expense
+const expenses = computed(() => {
+  return transactions.value
+    .filter((transaction) => transaction.amount < 0)
+    .reduce((acc, transaction) => {
+      return acc + transaction.amount;
+    }, 0)
+    .toFixed(2);
+});
+
+//addTransactions
+const handleTransactionSubmitted = (transactionData) => {
+  transactions.value.push({
+    id: generateUniqueId(),
+    text: transactionData.text,
+    amount: transactionData.amount,
+  });
+  saveTransactionsToLocalStorage();
+  toast.success("Transaction added");
+};
+
+const generateUniqueId = () => {
+  return Math.floor(Math.random() * 1000000);
+};
+
+const handleTransactionDeleted = (id) => {
+  transactions.value = transactions.value.filter(
+    (transaction) => transaction.id !== id
+  );
+  saveTransactionsToLocalStorage();
+  toast.success("Transaction deleted");
+};
+
+const saveTransactionsToLocalStorage = () => {
+  localStorage.setItem("transactions", JSON.stringify(transactions.value));
+};
 </script>
